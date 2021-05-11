d6cf76586b41b9f5cd1435e5526fdcb47130027b
s.elamraoui <s.elamraoui@agiradom.com>
2021-05-11 10:48:33 +0200

    modif icon stack

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -21,10 +21,10 @@ export class Tab2Page implements OnInit {
     for (let i = 0; i < this.cart.length; i++) {
       for (let j = 0; j < this.cart[i].cart.length; j++) {
         if (this.cart[i].cart[j].energyKcal100g) {
-          this.moyenneKcal += parseInt(this.cart[i].cart[j].energyKcal100g)
+          this.moyenneKcal += parseInt(this.cart[i].cart[j].energyKcal100g);
         }
-        if (j == this.cart[i].cart.length - 1) {
-          console.log(this.moyenneKcal)
+        if (j === this.cart[i].cart.length - 1) {
+          console.log(this.moyenneKcal);
           this.moyenneKcal = parseFloat((this.moyenneKcal / j).toFixed(2));
         }
       }
